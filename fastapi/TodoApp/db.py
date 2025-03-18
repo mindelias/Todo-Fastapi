5aec63eda2e7d4accd930a7f331d9af83a5d4fb1
mindelias <mindelias@gmail.com>
2025-03-18 13:56:58 +0100

    rename db

--- fastapi/TodoApp/db.py
+++ fastapi/TodoApp/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
